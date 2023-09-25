8f5b3a30645e327fb1a962fa5695287e835fb815
irysik123 <vysotskaya.iryna@gmail.com>
2023-09-25 15:06:57 +0400

    init project

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://irysik123:sOEZOuVSm1zEZrMQ@cluster0.xhrrcxy.mongodb.net/db-books?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    })
+  )
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
