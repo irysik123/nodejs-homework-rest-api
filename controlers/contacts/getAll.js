1ba9e6d568815443af63c3d2fe28ecc66770b188
irysik123 <vysotskaya.iryna@gmail.com>
2023-09-30 17:28:03 +0400

    fixing response for getting contacts

--- controlers/contacts/getAll.js
+++ controlers/contacts/getAll.js
@@ -1,11 +1,11 @@
-
-const Contact = require("../../models/db-contacts")
+const Contact = require("../../models/db-contacts");
 
 const getAll = async (req, res, next) => {
   const { _id: owner } = req.user;
-  const {page = 1, limit = 20} = req.query;
-  const skip = (page - 1) * limit;
-  let contacts = await Contact.find({owner}, {skip, limit}).populate("owner", "email");
+  let contacts = await Contact.find({ owner }).populate(
+    "owner",
+    "email"
+  );
   res.status(200).json(contacts);
 };
 
