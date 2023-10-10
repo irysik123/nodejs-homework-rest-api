a05ef89dda2787caa1a07209215b9b9d29edb6c4
irysik123 <vysotskaya.iryna@gmail.com>
2023-10-10 10:06:34 +0200

    fixing respose for request with no image

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,21 +1,15 @@
 const multer = require("multer");
 const path = require("path");
+const HttpError = require("./HttpError");
 
 const tempDir = path.join(__dirname, "../", "temp");
 
 const multerConfig = multer.diskStorage({
   destination: tempDir,
-})
+});
 
 const upload = multer({
-    storage: multerConfig,
-    fileFilter: function (req, file, cbk) {
-      if (file.mimetype.startsWith('image/')) {
-        cbk(null, true);
-      } else {
-        throw HttpError(400, "Please upload images only");
-      }
-    }
-})
+  storage: multerConfig,
+});
 
 module.exports = upload;
